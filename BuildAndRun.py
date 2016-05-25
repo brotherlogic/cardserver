14a8eeb4d575349b174686de7b02d3e7ad5ad366
Simon Tucker <brotherlogic@gmail.com>
2016-05-25 05:26:04 +0000

    updates for run script

---
+++ BuildAndRun.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+# Update to the latest version
+for line in os.popen('git fetch -p; git merge -q origin/master').readlines():
+    print line.strip()
+
+# Move the old version over
+for line in os.popen('cp cardserver oldcardserver').readlines():
+    print line.strip()
+
+# Rebuild
+for line in os.popen('go build ./...').readlines():
+    print line.strip()
+
+# Rebuild
+for line in os.popen('go build').readlines():
+    print line.strip()
+
+
+size_1 = os.path.getsize('./cardserver')
+size_2 = os.path.getsize('./oldcardserver')
+
+if size_1 != size_2:
+    for line in os.popen('killall cardserver').readlines():
+        print line.strip()
+    print subprocess.Popen('./cardserver')
