7c75e91984862ee06604f886b4e9bb424aa55d9d
Simon Tucker <brotherlogic@gmail.com>
2016-05-24 22:31:13 -0700

    Small code clean

--- card_server.go
+++ card_server.go
@@ -1,47 +1,46 @@
 package main
 
 import (
-       "log"
-       "net"
-       
-       "golang.org/x/net/context"
-       "google.golang.org/grpc"
-       pb "github.com/brotherlogic/cardserver/card"
+	"log"
+	"net"
+
+	pb "github.com/brotherlogic/cardserver/card"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 const (
-      port = ":50051"
+	port = ":50051"
 )
 
 type server struct {
-     cards *pb.CardList
+	cards *pb.CardList
 }
 
-func InitServer() (server) {
-     s := server{}
-     s.cards = &pb.CardList{}
+func InitServer() server {
+	s := server{}
+	s.cards = &pb.CardList{}
 
-     return s
+	return s
 }
 
-func (s *server) GetCards (ctx context.Context, in *pb.Empty) (*pb.CardList, error) {
-     return s.cards, nil
+func (s *server) GetCards(ctx context.Context, in *pb.Empty) (*pb.CardList, error) {
+	return s.cards, nil
 }
 
-func (s *server) AddCards (ctx context.Context, in *pb.CardList) (*pb.CardList, error) {
-     s.cards.Cards = append(s.cards.Cards, in.Cards...)
-     log.Printf("Received %v", s.cards.Cards)
-     return s.cards, nil
+func (s *server) AddCards(ctx context.Context, in *pb.CardList) (*pb.CardList, error) {
+	s.cards.Cards = append(s.cards.Cards, in.Cards...)
+	return s.cards, nil
 }
 
 func main() {
-     lis, err := net.Listen("tcp", port)
-     if err != nil {
-     	log.Fatalf("Failed to listen on %v", err)
-     }
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen on %v", err)
+	}
 
-     s := grpc.NewServer()
-     server := InitServer()
-     pb.RegisterCardServiceServer(s, &server)
-     s.Serve(lis)
+	s := grpc.NewServer()
+	server := InitServer()
+	pb.RegisterCardServiceServer(s, &server)
+	s.Serve(lis)
 }
