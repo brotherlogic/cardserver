e37c530a9e948a0d701df4b909d4d7548820ed33
Simon Tucker <brotherlogic@gmail.com>
2016-05-25 18:51:22 -0700

    Removes expired cards from the server. This closes #17.

--- card_server.go
+++ card_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 
 	pb "github.com/brotherlogic/cardserver/card"
 	"golang.org/x/net/context"
@@ -27,7 +28,21 @@ func (s *server) dedup() {
 	log.Printf("Deduping")
 }
 
+func (s *server) removeStaleCards() {
+
+	filteredCards := &pb.CardList{}
+	for _, card := range s.cards.Cards {
+		if card.ExpirationDate <= 0 || card.ExpirationDate >= time.Now().Unix() {
+			log.Printf("Not filtering %v (time is %v)", card, time.Now().Unix())
+			filteredCards.Cards = append(filteredCards.Cards, card)
+		}
+	}
+
+	s.cards = filteredCards
+}
+
 func (s *server) GetCards(ctx context.Context, in *pb.Empty) (*pb.CardList, error) {
+	s.removeStaleCards()
 	return s.cards, nil
 }
 
--- card_server_test.go
+++ card_server_test.go
@@ -1,76 +1,101 @@
 package main
 
 import (
-"net"
- "testing"
- "time"
- "golang.org/x/net/context"
- "google.golang.org/grpc"
- pb "github.com/brotherlogic/cardserver/card"
+	pb "github.com/brotherlogic/cardserver/card"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net"
+	"testing"
+	"time"
 )
 
 func TestAdd(t *testing.T) {
-     card := pb.Card{}
-     s := InitServer()
-
-     cardlist := pb.CardList{}
-     cardlist.Cards = append(cardlist.Cards, &card)
-
-     cards, err := s.AddCards(context.Background(), &cardlist)
-
-     if err != nil {
-     	t.Errorf("Error in adding a card: %v", err)
-     }
-
-     if len(cards.Cards) != 1 {
-     	t.Errorf("Not enough cards: %v", len(cards.Cards))
-     }
-}
-
-func TestRunServer(t *testing.T) {
-     go func() {
-     	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-	   t.Errorf("Error opening port up")
-	}
-	s := grpc.NewServer()
-	server := InitServer()
-	pb.RegisterCardServiceServer(s, &server)
-	s.Serve(lis)
-     }()
-
-     go func() {
-	time.Sleep(5 * time.Second)
-     	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-	if err != nil {
-	   t.Errorf("Error connecting to port")
-	}
-	defer conn.Close()
-	client := pb.NewCardServiceClient(conn)
-
 	card := pb.Card{}
-	card.Text = "Testing"
+	s := InitServer()
+
 	cardlist := pb.CardList{}
 	cardlist.Cards = append(cardlist.Cards, &card)
 
-	r, err := client.AddCards(context.Background(), &cardlist)
+	cards, err := s.AddCards(context.Background(), &cardlist)
+
 	if err != nil {
-	   t.Errorf("Error adding cards: %v", err)
+		t.Errorf("Error in adding a card: %v", err)
 	}
 
-	r, err = client.GetCards(context.Background(), &pb.Empty{})
+	if len(cards.Cards) != 1 {
+		t.Errorf("Not enough cards: %v", len(cards.Cards))
+	}
+}
+
+func TestRemoveStale(t *testing.T) {
+	card := pb.Card{ExpirationDate: time.Now().Unix() - 10}
+	s := InitServer()
+
+	cardlist := pb.CardList{}
+	cardlist.Cards = append(cardlist.Cards, &card)
+
+	cards, err := s.AddCards(context.Background(), &cardlist)
+
 	if err != nil {
-	   t.Errorf("Error getting cards: %v", err)
+		t.Errorf("Error adding card: %v", err)
 	}
-	if len(r.Cards) != 1 {
-	   t.Errorf("Not enough cards: %v", r)
-	}
-	if r.Cards[0].Text != "Testing" {
-	   t.Errorf("Card read is wrong: %v", r)
-	}
-     }()
 
-     
+	if len(cards.Cards) != 1 {
+		t.Errorf("Card has not beed added: %v", len(cards.Cards))
+	}
 
-     time.Sleep(10 * time.Second)
+	cards, err = s.GetCards(context.Background(), &pb.Empty{})
+	if err != nil {
+		t.Errorf("Error getting cards: %v", err)
+	}
+
+	if len(cards.Cards) != 0 {
+		t.Errorf("Card has not been removed: %v:%v", len(cards.Cards), cards.Cards)
+	}
+}
+
+func TestRunServer(t *testing.T) {
+	go func() {
+		lis, err := net.Listen("tcp", ":50051")
+		if err != nil {
+			t.Errorf("Error opening port up")
+		}
+		s := grpc.NewServer()
+		server := InitServer()
+		pb.RegisterCardServiceServer(s, &server)
+		s.Serve(lis)
+	}()
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			t.Errorf("Error connecting to port")
+		}
+		defer conn.Close()
+		client := pb.NewCardServiceClient(conn)
+
+		card := pb.Card{}
+		card.Text = "Testing"
+		cardlist := pb.CardList{}
+		cardlist.Cards = append(cardlist.Cards, &card)
+
+		r, err := client.AddCards(context.Background(), &cardlist)
+		if err != nil {
+			t.Errorf("Error adding cards: %v", err)
+		}
+
+		r, err = client.GetCards(context.Background(), &pb.Empty{})
+		if err != nil {
+			t.Errorf("Error getting cards: %v", err)
+		}
+		if len(r.Cards) != 1 {
+			t.Errorf("Not enough cards: %v", r)
+		}
+		if r.Cards[0].Text != "Testing" {
+			t.Errorf("Card read is wrong: %v", r)
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
 }
