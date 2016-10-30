b9b9d1abd3c91ed6280eebc386ac0c7b6a0262f8
Simon Tucker <brotherlogic@gmail.com>
2016-10-30 12:38:21 -0700

    Moving to process output

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -10,7 +10,7 @@ if os.path.isfile('hash'):
     current_hash = open('hash').readlines()[0]
 new_hash = os.popen('git rev-parse HEAD').readlines()[0]
 open('hash','w').write(new_hash)
-    
+
 # Move the old version over
 for line in os.popen('cp ' + name + ' old' + name).readlines():
     print line.strip()
@@ -28,10 +28,12 @@ size_2 = os.path.getsize('./' + name)
 
 running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
 
-              
+
 if size_1 != size_2 or new_hash != current_hash or not running:
     for line in os.popen('killall ' + name).readlines():
         pass
     for line in os.popen('cp out.txt out-' + `time.time()`).readlines():
         pass
-    subprocess.Popen(['./' + name + ' > out.txt'])
+    with open('out.txt', 'w') as output:
+        server = subprocess.Popen('./' + name, stdout=output, stderr=output)
+        server.communicate()
