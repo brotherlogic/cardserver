53afb4c0f91f375cf173362e762c22bc587f63c4
Simon Tucker <brotherlogic@gmail.com>
2017-04-11 21:07:25 -0700

    Updated for new goserver

--- card_server_network.go
+++ card_server_network.go
@@ -11,6 +11,11 @@ func (s *Server) DoRegister(server *grpc.Server) {
 	pb.RegisterCardServiceServer(server, s)
 }
 
+// ReportHealth Determines if the server is healthy
+func (s *Server) ReportHealth() bool {
+	return true
+}
+
 func main() {
 	server := InitServer()
 	server.PrepServer()
