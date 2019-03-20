e6f735c01658c2d693904fca7bbed42a33820752
Simon Tucker <simon@brotherlogic@gmail>
2019-03-19 18:13:04 -0700

    Fixes cardserver

--- card_server_network.go
+++ card_server_network.go
@@ -23,6 +23,11 @@ func (s *Server) ReportHealth() bool {
 	return true
 }
 
+// Shutdown the server
+func (s *Server) Shutdown(ctx context.Context) error {
+	return nil
+}
+
 // Mote promotes this server
 func (s *Server) Mote(ctx context.Context, master bool) error {
 	if master {
